42ff860b2e8b2f6c59c2a63f06dbff0879254e2d
Rishabh Mishra <rishabhm1508@gmail.com>
2024-09-17 20:07:56 +0530

    Update icon and remove unwanted images

--- youtube-clone/src/utilities/constants.js
+++ youtube-clone/src/utilities/constants.js
@@ -1,7 +1,7 @@
 export const hamburgerIconUrl =
   "https://cdn.iconscout.com/icon/free/png-256/free-hamburger-menu-icon-download-in-svg-png-gif-file-formats--crispy-user-interface-pack-icons-462145.png?f=webp&w=256";
 export const youtubeLogo =
-  "https://cdn.mos.cms.futurecdn.net/8gzcr6RpGStvZFA2qRt4v6.jpg";
+  "https://www.freeiconspng.com/thumbs/youtube-logo-png/youtube-logo-png-transparent-image-5.png";
 
 export const searchIcon =
   "https://i.pinimg.com/736x/fa/0e/7b/fa0e7b992eff03c576727e95c746005c.jpg";
